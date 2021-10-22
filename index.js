cabc926b5fbbe0e2f7436d391bb83731e58350a6
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-22 11:59:33 -0500

    query params and order in endpoints.
    Static endpoints have to be before dinamic endpoints.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const faker = require('faker')
+
 
 const app = express()
 
@@ -12,15 +14,40 @@ app.get('/new-route', (req, res) => {
     res.send('hello i am a new route or endpoint')
 
 })
+
 app.get('/products', (req, res) => {
-    res.json([
-        {
-        name: 'Product1',
-        price: 3000
-    }, {
-        name: 'Product2',
-        price: 2000
-    }])
+    const products = []
+    const {size} = req.query
+    const limit = size || 10
+    for (let index = 0; index<limit; index++){
+        products.push({
+            name: faker.commerce.productName(),
+            price: parseInt(faker.commerce.productName()),
+            image: faker.image.imageUrl(),
+        })
+    }
+    res.json(products)
+})
+
+
+
+
+// app.get('/products/:id', (req, res) => {
+//     //const id = req.params.id
+
+//     //Destrcuture
+//     const {id} = req.params
+//     res.json({
+//         id,
+//         name: 'Product2',
+//         price: 2000
+//     })
+
+// })
+
+//filter its get taken as an id. Specfici endpoints has to go beforer dinamic endpoints.
+app.get('/products/filter', (req, res) => {
+    res.send('i m filter')
 
 })
 
@@ -37,6 +64,20 @@ app.get('/products/:id', (req, res) => {
 
 })
 
+app.get('/users', (req, res) => {
+    const {limit, offset} = req.query
+
+    if(limit && offset){
+        res.json({
+            limit,
+            offset
+        })
+
+    } else {
+        res.send('No params')
+    }
+
+})
 
 app.get('/categories/:categoryId/products/:productId', (req, res) => {
     //const id = req.params.id
