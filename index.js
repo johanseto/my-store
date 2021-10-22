7fdc1096b6b8b6d688358f845005efcc540acbf0
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-21 22:32:55 -0500

    configure express server and routes.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('My App');
+const express = require('express')
+
+const app = express()
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.send('hello server in express')
+})
+
+app.get('/new-route', (req, res) => {
+    res.send('hello i am a new route or endpoint')
+
+})
+app.get('/products', (req, res) => {
+    res.json({
+        name: 'Product1',
+        price: 3000
+    })
+
+})
+
+
+app.listen(port, () => {
+    console.log("my port" + port)
+})
