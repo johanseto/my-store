29ef53ef9a28c65fec4323ab08c393505387a602
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-23 11:38:15 -0500

    using classes for services.
    Take care new isntance is saved in memory while the script or server is
    running.
    This gives an memory persistence idea.
    This because in products.router the class is geneate as a global
    variable, while all the mehtod are rerunning by the app of express..;
    The service class was created an stored like a global variable.

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,26 +1,15 @@
 const express = require('express')
-const faker = require('faker')
+const ProductsService = require('./../services/products.service')
 
 const router = express.Router()
+const service = new ProductsService()
 
 router.get('/', (req, res) => {
-    const products = []
-    const {size} = req.query
-    const limit = size || 10
-    for (let index = 0; index<limit; index++){
-        products.push({
-            name: faker.commerce.productName(),
-            price: parseInt(faker.commerce.productName()),
-            image: faker.image.imageUrl(),
-        })
-    }
+    const products = service.find()
     res.json(products)
+ 
 })
-
-
-
-
-// app.get('/products/:id', (req, res) => {
+// app.get('/:id', (req, res) => {
 //     //const id = req.params.id
 
 //     //Destrcuture
@@ -44,20 +33,17 @@ router.get('/:id', (req, res) => {
 
     //Destrcuture
     const {id} = req.params
-    res.json({
-        id,
-        name: 'Product2',
-        price: 2000
-    })
+
+    const product = service.findOne(id)
+
+    res.json(product)
 
 })
 
 router.post('/',(req, res) => {
     const body = req.body
-    res.json({
-        message: 'created',
-        data: body,
-    })
+    const newProduct = service.create(body)
+    res.status(201).json(newProduct)
 })
 
 
