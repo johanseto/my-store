cfce528af36386b6d0c7ff213b95bb96a54e1c0a
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-23 11:01:19 -0500

    post method for products
    Remember to use the middleware of express
    to uses json in index.js

--- routes/products.router.js
+++ routes/products.router.js
@@ -52,4 +52,23 @@ router.get('/:id', (req, res) => {
 
 })
 
+router.post('/',(req, res) => {
+    const body = req.body
+    res.json({
+        message: 'created',
+        data: body,
+    })
+})
+
+
+router.post('/:id',(req, res) => {
+    const{ id } = req.params
+    const body = req.body
+    res.json({
+        message: 'created',
+        data: body,
+        id,
+    })
+})
+
 module.exports = router
