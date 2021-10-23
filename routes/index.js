96f21d53e1e27ac4fb95dda1209ffe5797b95d87
johan.castiblanco <johan.castiblanco@pevaar.com>
2021-10-23 10:49:36 -0500

    add versions to the api with routes.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,11 @@ const productRouter = require('./products.router')
 const usersRouter = require('./users.router')
 const categoriesRouter = require('./categories.router')
 function routerApi(app) {
-    const router = express.router()
-    app.use('/products', productRouter)
-    app.use('/users', usersRouter)
-    app.use('/categories', categoriesRouter)
+    const router = express.Router()
+    app.use('/api/v1', router)
+    router.use('/products', productRouter)
+    router.use('/users', usersRouter)
+    router.use('/categories', categoriesRouter)
 }
 
 module.exports  = routerApi
